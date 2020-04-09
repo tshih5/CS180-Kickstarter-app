acb5ede4f454018db3358e28cd749204ca4287ec
Tom Shih <tshih98@gmail.com>
2020-04-08 19:56:08 -0700

    Added basic website layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import { Button } from 'antd';
 import './App.css';
+import React from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
 
 const App = () => (
   <div className="App">
-    <Button type="primary">Button</Button>
+     <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-content">Content</div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>CS180 Project by CSgods</Footer>
+    </Layout>
   </div>
 );
 
