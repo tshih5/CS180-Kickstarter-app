cfb646830352c6405009d6aa8e2b251536029af6
bchan023 <bchan023@ucr.edu>
2020-04-23 22:07:22 -0700

    backend api

---
+++ api/datasetpoint.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+
+var data = require('./data/dataset.json')
+
+
+app.get('/home', function (req, res) {
+	res.send(data);
+})
+
+app.get("/search/:searchtext", (req, res) => {
+	console.log(req.params)
+	
+    if (req.params["searchtext"] == "undefined") {
+	res.send(data)}
+	else {
+		searchtext = req.params["searchtext"].toLowerCase();
+		matches = [];
+		for(i = 0; i < data.length; i++) {
+			if (data[i].name.toLowerCase().includes(searchtext)) {
+				matches.push(data[i])
+			}
+		
+		}	
+		console.log("returning these" + matches)
+		res.send(JSON.stringify(matches));
+	}
+});
+
+
+//app.post("/update/", (req,res) => {
+	
+
+app.listen(2345, () => {
+	console.log("server is work");
+	
+});
+
