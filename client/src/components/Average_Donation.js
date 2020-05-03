1ec0011165f1d53063501fdf81ed66530cb5c8da
Samuel Liu <sliu055@ucr.edu>
2020-05-03 14:18:00 -0700

    minor changes

--- client/src/components/Average_Donation.js
+++ client/src/components/Average_Donation.js
@@ -1,5 +1,6 @@
 import { Select, Modal, Button } from 'antd';
 import React, {Component} from 'react';
+import axios from "axios";
 
 const { Option } = Select;
 
@@ -7,7 +8,7 @@ export default class PopUp2 extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {visible: false, value: "Project Category"};
+    this.state = {visible: false, value: "Project Category", donation: 0};
         
     this.handleChange = this.handleChange.bind(this);
     this.handleOk = this.handleOk.bind(this);
@@ -23,14 +24,31 @@ export default class PopUp2 extends Component {
 
   handleOk = e => {
     //route to calculation function?
-   this.setState({
-       visible: false,
-   });
+   console.log("value: ", this.state.value);
+   var values = {"value": this.state.value, "donation": 0}
+   this.getDonation(values);
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
   };
 
   handleChange = (id, e) =>{
       this.setState({value: e}); 
   };
+
+  dropDownValue(value) {
+      console.log(value);
+  }
+
+  getDonation = async values => {
+      const result = await axios.post("http://localhost:9000/datasetpoint/getDonation", values);
+      console.log(typeof result.data);
+      this.setState({donation: result.data});
+  }
 
   render() {
     return (
@@ -42,9 +60,11 @@ export default class PopUp2 extends Component {
           title="Average Amount of Money Raised by Category"
           visible={this.state.visible}
           onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          okText="Calculate"
         >
           <div>
-            <Select value={this.state.value} onChange="">
+            <Select value={this.state.value} onChange={(e) => this.handleChange(0, e)}>
               <Option value="Music">Music</Option>
               <Option value="Food">Food</Option>
               <Option value="Design">Design</Option>
@@ -56,6 +76,9 @@ export default class PopUp2 extends Component {
               <Option value="Crafts">Crafts</Option>
               <Option value="Art">Art</Option>
             </Select>
+          </div>
+          <div>
+              <p>The average donation of {this.state.value} is ${this.state.donation} </p>
           </div>
         </Modal>
       </div>
